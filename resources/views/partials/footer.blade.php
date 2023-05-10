425fb45a78969ec23529b4c043b7b5a2d14eb1b0
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-11 01:24:59 +0200

    Fine esercizio

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -48,4 +48,27 @@
             </div>
         </div>
     </div>
+
+    <div class="my_footer2 d-flex align-items-center">
+        <div class="container w-75 m-auto ">
+            <div class="row">
+                <div class="col">
+                    <button class="btn btn-primary me-5">SIGN UP NOW</button>
+
+                </div>
+                <div class="col">
+                    <div class="d-flex align-items-center">
+
+                        <h5 class="text-primary">FOLLOW US</h5>
+                        <i class="fa-brands fa-facebook ms-3 fs-3 my_icon"></i>
+                        <i class="fa-brands fa-twitter ms-3 fs-3 my_icon"></i>
+                        <i class="fa-brands fa-youtube ms-3 fs-3 my_icon"></i>
+                        <i class="fa-brands fa-google ms-3 fs-3 my_icon"></i>
+                        <i class="fa-brands fa-dropbox fa-google ms-3 fs-3 my_icon"></i>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
