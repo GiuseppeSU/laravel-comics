da75c87b87d75648193d2f9f47fb7fff5b43d7b5
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-10 16:46:05 +0200

    Commit di aggiornamento

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,25 @@
 <header>
 
-    <h1>qui ci va l'header</h1>
+    <div class="navbar">
+        <div class="nav-logo">
+            <img src="" alt=" Dc logo">
+        </div>
+        <div class="link-nav">
+            <ul class="list">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Còllectibles</li>
+                <li>Video</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+
+            </ul>
+        </div>
+    </div>
+
 
 </header>
