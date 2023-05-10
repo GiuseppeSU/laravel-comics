a0006d64dd6ace4e0fb728867790dcc04655499b
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-10 17:19:30 +0200

    Fine header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,43 @@
 <header>
-
-    <div class="navbar">
-        <div class="nav-logo">
-            <img src="" alt=" Dc logo">
+    <div class="container-little">
+        <div class="little-text">
+            <p>DC POWER VISA</p>
+            <p>ADDITIONAL DC SITE<i class="fa-solid fa-caret-down"></i></p>
         </div>
-        <div class="link-nav">
-            <ul class="list">
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Còllectibles</li>
-                <li>Video</li>
-                <li>Fans</li>
-                <li>News</li>
-                <li>Shop</li>
 
-            </ul>
-        </div>
+
+
     </div>
+    <div class="container">
+
+
+        <div class="navbar">
+            <div class="nav-logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt=" Dc logo">
+            </div>
+            <div class="link-nav">
+                <ul class="list">
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Còllectibles</li>
+                    <li>Video</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+
+                </ul>
+            </div>
+            <div class="search">
+                <p>Search<i class="fa-solid fa-magnifying-glass"></i></p>
+            </div>
+        </div>
+
+    </div>
+
+
 
 
 </header>
